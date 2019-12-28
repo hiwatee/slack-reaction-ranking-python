39e8f4a5a04344bf8571b472d4b3ef598b673dcc
Hiwatashi Masayuki <m-hiwatawshi@kakusin-g.co.jp>
2019-12-28 15:31:03 +0900

    コード追加

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import json
+import requests
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+def main():
+    # .envから環境変数を読み込み
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    token = os.environ.get("TOKEN")
+    host = os.environ.get("HOST")
+    # ランキング初期化
+    ranking = {}
+    # チャンネルリストを取得・整形
+    url = host + 'channels.list?token=' + token + '&exclude_archived=true'
+    r = requests.get(url)
+    data = r.json()
+    channels = [{'id': channel['id'], 'name': channel['name']}
+                for channel in data['channels']]
+    for channel in channels:
+        # 各チャンネルから上位1000件を取得する
+        url = host + 'channels.history?token=' + \
+            token + '&channel=' + channel['id'] + '&count=1000'
+        r = requests.get(url)
+        data = r.json()
+
+        # reactionを取得
+        for message in data['messages']:
+            try:
+                for reaction in message['reactions']:
+                    if not ranking[reaction['name']]:
+                        ranking[reaction['name']] = reaction['count']
+                    else:
+                        ranking[reaction['name']] += reaction['count']
+            except KeyError:
+                pass
+    # 並び替え
+    ranking_sorted = sorted(
+        ranking.items(), key=lambda x: x[1], reverse=True)
+    # ターミナルでは収まりきらないのでファイルに保存する
+    with open(dirname(__file__) + 'output.txt', mode='w') as f:
+        f.write(json.dumps(ranking_sorted))
+
+
+if __name__ == '__main__':
+    main()
